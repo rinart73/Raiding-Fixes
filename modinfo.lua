b5a78762fc0a7c3853271095b5f9aa5cdda6bdbb
Rinart73 <rinart73@gmail.com>
2020-01-23 22:43:30 +0100

    Pushed supported version to 0.30.2

--- modinfo.lua
+++ modinfo.lua
@@ -40,7 +40,7 @@ meta =
     -- },
     dependencies = {
         {id = "1722652757", min = "1.2"},
-        {id = "Avorion", min = "0.25.2", max = "0.29.6"}
+        {id = "Avorion", min = "0.25.2", max = "0.30.2"}
     },
 
     -- Set to true if the mod only has to run on the server. Clients will get notified that the mod is running on the server, but they won't download it to themselves
